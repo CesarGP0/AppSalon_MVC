ba3b3b0575cb0791bde2a1400080aa5fbcb1e9d4
Cesar <c.guillermop@outlook.com>
2022-11-05 00:20:24 -0600

    Deployment de la app

--- Router.php
+++ Router.php
@@ -28,7 +28,7 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
